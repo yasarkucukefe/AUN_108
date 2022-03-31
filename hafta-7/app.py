620a273141ec11571ed417bb7ba343633d43122a
yasarkucukefe <yasar.kucukefe@gmail.com>
2022-03-31 16:31:20 +0300

    7. hafta

---
+++ hafta-7/app.py
@@ -0,0 +1,12 @@
+
+def main():
+    print_merhaba()
+
+def print_merhaba():
+    print("Merhaba")
+
+# print("app.py output:"+ __name__)
+#print_merhaba()
+
+if(__name__ == "__main__"):
+    main()
---
+++ hafta-7/app2.py
@@ -0,0 +1,3 @@
+from app import *
+
+#print_merhaba()
